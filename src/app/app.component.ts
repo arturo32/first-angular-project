6ab8f31d9eac66136ef4a4c38eba620241a9b822
Arturo Fonseca <arturofonsecas32@gmail.com>
2023-08-24 21:57:05 -0300

    Create app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+interface myFirstType {
+  name: string,
+  age: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   title = 'first-angular-project';
+  firstVar: myFirstType = {
+    name: "Arturo",
+    age: 24
+  }
+  colors: string[] = ['green', 'red', 'yellow']
 }
